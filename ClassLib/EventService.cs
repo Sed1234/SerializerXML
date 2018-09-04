2c4c49fd763043c4eec24f8df6874f9f22b82fb6
КимС <@224_10>
2018-09-04 22:08:51 +0600

    XML

--- ClassLib/EventService.cs
+++ ClassLib/EventService.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Xml.Serialization;
+using System.IO;
+using ClassLib;
+
 
 namespace ClassLib
 {
-    class EventService
+    public class EventService
     {
         public static void DoEvent(EventPCLog log, PC pc)
         {
@@ -14,7 +17,10 @@ namespace ClassLib
             logPC.pc = pc;
             logPC.EventDate = DateTime.Now;
 
-
+            XmlSerializer formater = new XmlSerializer(logPC.GetType());
+            FileStream fs = new FileStream(DateTime.Now.Ticks + ".xml", FileMode.OpenOrCreate);
+            formater.Serialize(fs, log);
+            fs.Close();
 
         }
     }
--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClassLib;
+
 
 namespace ConsoleApp4
 {
@@ -10,12 +12,14 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
-            SerializeSOAP SP = new SerializeSOAP(@"C:\Users\КимС\Desktop\MOCK.csv");
-            SP.SerializationFile();
-            foreach(Person a in SP.DeserializeFile())
-            {
-                Console.WriteLine("Name - {0} Phone - {1}", a.Name, a.Phone);
-            }
+            //SerializeSOAP SP = new SerializeSOAP(@"C:\Users\КимС\Desktop\MOCK.csv");
+            //SP.SerializationFile();
+            //foreach(Person a in SP.DeserializeFile())
+            //{
+            //    Console.WriteLine("Name - {0} Phone - {1}", a.Name, a.Phone);
+            //}
+
+            EventService.DoEvent(EventPCLog.ON, ClassLib.PC("HP", "S123!", "ffx800", 1500));
         }
     }
 }
